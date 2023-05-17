5a4c1a0a1868e2202d982cec7e7654d037e21d5c
coder-faraz <hfaraz316@gmail.com>
2023-05-17 13:46:53 +0530

    sidebar topbar and header setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/Topbar.jsx";
+import Sidebar from "./scenes/global/Sidebar.jsx";
+import Dashboard from "./scenes/dashboard";
 
 function App() {
   const [theme, colorMode] = useMode();
 
   return (
-    <ColorModeContext.Provider value={ colorMode }>
-      <ThemeProvider theme={ theme }>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-          <div className="app">
-            <main className="content"></main>
-          </div>
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route exact path="/" element={ <Dashboard /> } />
+            </Routes>
+          </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
